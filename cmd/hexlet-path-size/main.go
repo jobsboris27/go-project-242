ec6cd5a0d367cf0403e188a14a2eb6da3ba576be
Boris <jobsboris27@yandex.ru>
2025-08-17 23:29:57 +0300

    feat: make step 2

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v3"
 )
 
 func main() {
-	fmt.Println("Hello from Hexlet!")
+	cmd := &cli.Command{
+		Name:      "hexlet-path-size",
+		Usage:     "print size of a file or directory",
+		UsageText: "hexlet-path-size [global options]",
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
--- path_size.go
+++ path_size.go
@@ -1,1 +1,37 @@
 package pathsize
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v3"
+)
+
+func Run() {
+	cmd := &cli.Command{
+		UseShortOptionHandling: true,
+		Commands: []*cli.Command{
+			{
+				Name:  "short",
+				Usage: "complete a task on the list",
+				Flags: []cli.Flag{
+					&cli.BoolFlag{Name: "serve", Aliases: []string{"s"}},
+					&cli.BoolFlag{Name: "option", Aliases: []string{"o"}},
+					&cli.StringFlag{Name: "message", Aliases: []string{"m"}},
+				},
+				Action: func(ctx context.Context, cmd *cli.Command) error {
+					fmt.Println("serve:", cmd.Bool("serve"))
+					fmt.Println("option:", cmd.Bool("option"))
+					fmt.Println("message:", cmd.String("message"))
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
